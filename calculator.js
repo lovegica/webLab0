e2626c7bab332890c4e0fd2826975b01fc04b507
lovegica <rosewwoo@gmail.com>
2015-11-11 17:36:04 +0900

    Update calculator.js
    잘못된파일 업로드로인해 다시 업로드합니다.

--- calculator.js
+++ calculator.js
@@ -1,69 +1,95 @@
+
 "use strict"
 window.onload = function () {
     var stack = [];
-    var displayVal = "0";
+    var displayVal = "0"   ;
     for (var i in $$('button')) {
         $$('button')[i].onclick = function () {
-            //var value = this.innerHTML;
-            var input = document.querySelector('.screen');
-            var inputVal = input.innerHTML;
-            var btnVal = this.innerHTML;
+            var value = this.innerHTML;
+            var languageCheck = /[0-9]/;
 
-            if(btnVal == 'AC'){
-                input.innerHTML = '';
-                decimalAdded = false;
-            }else if(btnVal == '=') {
-                var equation = inputVal;
-                var lastChar = equation[equation.length - 1];
+            if (languageCheck.test(value)) {
+
+                if (displayVal =="0") {
+                     displayVal = value;
+                }
+                else{
+                    displayVal = displayVal+value;
+                }
+                stack.push(displayVal);
+                document.getElementById('result').innerHTML = displayVal; 
+            }else if (value =="AC") {
+                displayVal = "0";
+                stack = [];
+                document.getElementById('result').innerHTML = "0";
+                document.getElementById('expression').innerHTML = "0";
             }
-            equation = equation.replace(/x/g, '*').replace(/÷/g, '/');
-
-            if(operators.indexOf(lastChar) > -1 || lastChar == '.')
-                equation = equation.replace(/.$/, ''); 
-            if(equation)
-                input.innerHTML = eval(equation);
+            else if (value =='.') {
                 
-            decimalAdded = false;
-        }
+                if(displayVal.charAt(displayVal.length-1) =='.')
+                {
+                }
+                else{
+                     displayVal = displayVal+'.';
+                }
+                stack.push(displayVal);
+                document.getElementById('result').innerHTML = displayVal;
+            }
+            else if (value == '='){
+
+            }
+            else if (value == '!'){
+                var fact;
+                if(displayVal.charAt(displayVal.length-1) =='!')
+                {
+                }
+                else{
+                    fact = factorial(parseInt(displayVal));
+                    displayVal = displayVal+'!';
+                     
+                }
+                 document.getElementById('expression').innerHTML = fact;
+                 document.getElementById('result').innerHTML = displayVal;     
+            }
+            else{
+                if(displayVal.charAt(displayVal.length-1) == value)
+                {
+                }
+                else{
+                     displayVal = displayVal+value;
+                }
+                
+                document.getElementById('expression').innerHTML = displayVal; 
+                document.getElementById('result').innerHTML = "0";
+               
+                if (value == "*" || value == "/"  ||value == "^" ){
+                    highPriorityCalculator();
+                }
+               
+            }
+        };
+    }
+
 };
 
-function factorial (x) {
-    if ((x == 0) || (x == 1))
-        return 1
-    else{
-      result = (x * factorial(x-1) )
-      return result
-   }
-
-}
-function highPriorityCalculator(s, val) {
-
-}
 function calculator(s) {
     var result = 0;
-    var operators = ['+', '-', 'x', '÷'];
-    var decimalAdded = false;
-    for (var i=0; i< s.length; i++) { 
-        var btnVal = this.innerHTML; 
-        if(btnVal == '=') {
-            var equation = inputVal;
-            var lastChar = equation[equation.length - 1];
-            };
-    };
-
-    document.calculator.Display.value = result;
+    var operator = "+";
+    for (var i=0; i< s.length; i++) {
+        
     }
     return result;
 }
 
-function Dot() {
-    if ( Current.length == 0){ 
-        Current = "0.";
-    }else{  
-        if ( Current.indexOf(".") == -1){ 
-            Current = Current + ".";
-        };   
-    };
-  document.Calculator.Display.value = Current;
- }
 
+function factorial (x) {
+   var x;
+    for (var i = x; i == 1; i--) {
+        x = x * i;
+    };
+    return x;
+}
+function highPriorityCalculator(s, val) {
+
+
+}
