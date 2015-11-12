761ae10f9276d3771e5e8af9331797bbe674da56
lovegica <rosewwoo@gmail.com>
2015-11-12 21:20:47 +0900

    Update calculator_wed.js

--- calculator_wed.js
+++ calculator_wed.js
@@ -1,109 +1,136 @@
 "use strict"
 
-window.onload = function () {
+window.onload = function () {    
     var stack = [];
     var displayVal = "0";
+    var expressVal = "0";
+    
     for (var i in $$('button')) {
         $$('button')[i].onclick = function () {
             var value = this.innerHTML;
-            if(stack[stack.length-1]=="=") {
-                stack = [];
-                displayVal = "0";
-                document.getElementById('expression').innerHTML = "0";
-            }
             if(value=="AC") {
                 displayVal = "0";
+                expressVal = "0";
                 stack = [];
-                document.getElementById('expression').innerHTML = "0";
-            } else if(/^\d$/.test(value)) {
-                if(stack[stack.length-1]=="!") {
-                    alert("ERROR : input operator__ right after input !(factorial)");
-                } else {
-                    if(displayVal.charAt(0)=="0") {
-                        displayVal = value;
-                    } else {
-                        displayVal += value;
-                    }
+            }
+            else if(value==".") {
+                if(displayVal.split(".")[1]==undefined && /^[0-9]+$/.test(displayVal.split(".")[0])) {
+                    displayVal = displayVal + ".";
                 }
-            } else if(value==".") {
-                if(!(/.\.?./.test(displayVal))) {
-                    displayVal += value;
-                }
-            } else {
-                if(stack[stack.length-1]=="!") {
-                    stack.pop();
+            }       
+            else if(/^[0-9]+$/.test(value)){
+                if(displayVal=="0") {
                     displayVal = "";
-                } else {
-                   stack.push(parseFloat(displayVal));
                 }
-                var displayExp;
-                displayExp = displayVal + value;
+                displayVal = displayVal + value;
+            }
+            else if(value=="=") {
+                if(stack[stack.length-1]=="*" || stack[stack.length-1]=="/" || stack[stack.length-1]=="^") {
+                    highPriorityCalculator(stack, displayVal);
+                //      stack.push(parseFloat(displayVal));
+                // stack.push(value);
+                // displayVal = "0";
+                }
+                var sum = stack[0];
+                stack.push(parseFloat(displayVal));
+                for(var i = 0; i < stack.length; i++) {
+                    if(!/^[0-9]+$/.test(stack[i])) {
+                        if(stack[i]=="+") {
+                            sum = sum + stack[i+1];
+                        }
+                        else if(stack[i]=="-") {
+                            sum = sum - stack[i+1];
+                        }
+                        else if(stack[i]=="*") {
+                            sum = sum * stack[i+1];
+                        }
+                        else if(stack[i]=="^") {
+                            for(var j = 0; j < stack[i+1]-1; j++) {
+                                sum = sum * stack[0];
+                            }
+                        }                   
+                        
+                    }
 
-                if(document.getElementById('expression').innerHTML.charAt(0)=="0") {
-                    document.getElementById('expression').innerHTML = displayExp;                    
-                } else {
-                    document.getElementById('expression').innerHTML += displayExp;    
                 }
+                if (expressVal=="0") {
+                    expressVal = "";
+                }
+                expressVal = expressVal + displayVal;
+                displayVal = sum;
+            }
+            else if(value=="!") {
+                if (expressVal=="0") {
+                    expressVal = "";
+                }
+                expressVal = expressVal + displayVal + value;  
+                displayVal = factorial(displayVal);
+            }              
+            else {
+                if (expressVal=="0") {
+                    expressVal = "";
+                }
+                
+                if (expressVal.charAt(expressVal.length-1)=="!") {
+                    expressVal = expressVal + value;
+                }
+                else {
+                    expressVal = expressVal + displayVal + value; 
+                }
+                        
+                if(stack[stack.length-1]=="*" || stack[stack.length-1]=="/" || stack[stack.length-1]=="^") {
+                    highPriorityCalculator(stack, displayVal);
+                }
+                
+                stack.push(parseFloat(displayVal));
+                stack.push(value);
                 displayVal = "0";
-                if(/(\/|\*|\^)/.test(stack[stack.length-2])) {
-                    highPriorityCalculator(stack, value);
-                }
-                if(value=="!") {
-                    stack.push(parseFloat(factorial(stack.pop())));
-                    stack.push(value);
-                } else if(value=="=") {
-                    displayVal = calculator(stack);
-                    stack.push(value);
-                } else {
-                    stack.push(value);
-                }
             }
             document.getElementById('result').innerHTML = displayVal;
+            document.getElementById('expression').innerHTML = expressVal;
         };
     }
 };
 
-function factorial (x) {
-    if(x==1) {
-        return 1;
-    } else {
-        return x * factorial(x-1);
+
+function factorial(x) {
+    var value_ = 1;
+    for(var i=1; i<=x; i++) {
+        value_ = value_ * i;
+    }
+    return value_;
+
+}
+
+// r(var i=1; i<=x; i++) {
+//         value_ = value_ * i;
+//     }
+//     return value_;
+
+
+function highPriorityCalculator(s, val) {
+    var operator = s.pop();
+    var num_ = s.pop();
+    if(operator=="*") {
+        s.push(num_*parseFloat(val));
+    }
+    else if(operator=="/") {
+        s.push(num_/parseFloat(val));
+    }
+    else {
+        var value_ = 1;
+        for(var i = 1; i <= val; i++) {
+            value_ = value_ * num_;
+        }       
+        s.push(value_);
     }
 }
 
 function calculator(s) {
     var result = 0;
-    var operator__ = "+";
-
-    s.reverse();
-
-    var it = s.length;
-    for (var i=0; i< it; i++) {
-        if(i%2==0) { 
-            if(operator__ == "+") {
-                result = result + parseFloat(s.pop());
-            } else if(operator__ == "-") {
-                result = result - parseFloat(s.pop());
-            }
-        } else { 
-            operator__ = s.pop();
-        }
+    var operator = "+";
+    for (var i=0; i< s.length; i++) {
+        //push.
     }
     return result;
-}
-
-function highPriorityCalculator(s, val) {
-    var oper = s.pop();
-    var operator__ = s.pop();
-    var oper_ = s.pop();
-
-    var result;
-    if(operator__=="*") {
-        result = oper_ * oper;
-    } else if(operator__=="/") {
-        result = oper_ / oper;
-    } else if(operator__=="^") {
-        result = Math.pow(oper_, oper);
-    }
-    s.push(parseFloat(result));
 }
